7e53d84bd1fd29e23e7bc1da2995628258e8cd53
Romuald GRIGNON <romuald.grignon@gmail.com>
2020-08-06 19:03:31 +0200

    release v2.0.2 (still some problems using IntelliJ but it works using the script files)

--- resources/run_windows.bat
+++ resources/run_windows.bat
@@ -1,2 +1,2 @@
-java -cp *;students/* -Djava.library.path=lib fr.rphstudio.codingdojo.launcher.MainLauncher -noprint
+"C:\Program Files\java\jdk-14.0.1\bin\java.exe" -cp *;students/* -Djava.library.path=lib fr.rphstudio.codingdojo.launcher.MainLauncher -noprint -window
 pause

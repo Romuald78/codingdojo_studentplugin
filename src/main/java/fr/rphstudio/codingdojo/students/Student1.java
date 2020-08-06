217292bc17e59b4d8b24b59bf783993624c6f394
Romuald GRIGNON <romuald.grignon@gmail.com>
2020-08-06 19:09:13 +0200

    Student 1 now moveAndRecharge()

--- src/main/java/fr/rphstudio/codingdojo/students/Student1.java
+++ src/main/java/fr/rphstudio/codingdojo/students/Student1.java
@@ -5,6 +5,7 @@
  */
 package fr.rphstudio.codingdojo.students;
 
+
 import fr.rphstudio.codingdojo.game.Pod;
 import fr.rphstudio.codingdojo.game.PodPlugIn;
 
@@ -12,7 +13,7 @@ import fr.rphstudio.codingdojo.game.PodPlugIn;
  *
  * @author Romuald GRIGNON
  */
-public class Student1 extends PodPlugIn {    
+public class Student1 extends PodPlugIn {
     public Student1(Pod p){
         super(p);
     }
@@ -35,7 +36,7 @@ public class Student1 extends PodPlugIn {
         selectShip(1);
         setPlayerColor(255,255,255,255);
 
-        moveToNextCheckPoint(0.5f);
+        moveAndRecharge(0.75f, 20, 85);
 
         // END OF CODE AREA
         //-------------------------------------------------------
